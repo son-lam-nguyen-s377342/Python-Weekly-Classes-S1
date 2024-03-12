ebbceffe5ea267ddc8ffc39d174a054f218c6f7f
Son Lam <s377342@students.cdu.edu.au>
2024-03-12 21:29:32 +0930

    update

---
+++ Week 1/Listprogram.py
@@ -0,0 +1,6 @@
+array1 = [ 1, 3, 5, 7, 9]
+print(array1) # output- [1, 3, 5, 7, 9]
+print(array1[0]) # array1 with index 0 –output 1
+print(array1[1])
+print(array1[-1])
+print(array1[-4:]) # slicing returns a new list [3, 5, 7, 9]
---
+++ Week 1/Variableprogram.py
@@ -0,0 +1,5 @@
+width=10
+height=5
+total=width*height
+print("the total is", total)
+print("the height is", height,"\nthe width is",width)
