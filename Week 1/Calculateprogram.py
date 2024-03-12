19874403d6c00d8644140477be78b245df3495c5
Son Lam <s377342@students.cdu.edu.au>
2024-03-12 21:16:31 +0930

    Update

---
+++ Week 1/Calculateprogram.py
@@ -0,0 +1,6 @@
+
+print(2+4) # printing addition of 2 numbers – note this is a comment only
+print(20-3*4)
+print(7/2)
+print(2**2)
+print(13//2)
---
+++ Week 1/Firstprogram.py
@@ -0,0 +1,4 @@
+
+print("My First Program")
+print("Programming is fun")
+
