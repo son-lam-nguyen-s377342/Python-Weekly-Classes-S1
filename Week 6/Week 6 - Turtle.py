e6514e51a37819d9f47f2aef73cebb22cc57a1fd
Son Lam <s377342@students.cdu.edu.au>
2024-04-17 10:30:54 +0930

    update
    update

---
+++ Week 6/Week 6 - Turtle.py
@@ -0,0 +1,10 @@
+
+
+import turtle 
+
+t = turtle.Turtle()
+
+for i in range(100):
+    t.forward(i)
+    t.right(90)
+turtle.done()
---
+++ Week 6/Week 6 - Turtle2.py
@@ -0,0 +1,15 @@
+
+import turtle
+from turtle import *
+shape("turtle")
+def draw_square():
+    for i in range(0,4):
+        turtle.fd(50)
+        turtle.lt(90)
+        turtle.fd(75)
+
+def triangle():
+    for i in range(3):
+        turtle.fd(50)
+        turtle.lt(120)
+        turtle.fd(75)
